4e354c2353208a795437f6abecc27d1702742183
ruavee <saschavinnik06@mail.ru>
2025-09-22 01:32:47 +0300

    register and hash with some tests

---
+++ server/src/main/kotlin/org/interns/project/users/model/User.kt
@@ -0,0 +1,19 @@
+package org.interns.project.users.model
+
+import java.time.Instant
+
+data class User(
+    val id: Long,
+    val email: String,
+    val login: String,
+    val passwordHash: String,
+    val role: String = "CLIENT",
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val patronymic: String? = null,
+    val phoneNumber: String? = null,
+    val isActive: Boolean = true,
+    val clinicId: Long? = null,
+    val createdAt: Instant = Instant.now(),
+    val updatedAt: Instant = Instant.now()
+)
