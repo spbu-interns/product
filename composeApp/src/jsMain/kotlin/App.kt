25b669d963f1b9e02537aacfba45e096159d84cd
Ivan Barabanov <st128433@student.spbu.ru>
2025-09-30 00:00:32 +0300

    [front] added home screen and home button (#18)
    * added basic home screen and home button
    
    ---------
    
    Co-authored-by: Nether <4wows4nether@gmail.com>

--- composeApp/src/jsMain/kotlin/App.kt
+++ composeApp/src/jsMain/kotlin/App.kt
@@ -4,11 +4,12 @@ import io.kvision.panel.root
 import ui.registrationScreen
 import ui.stubScreen
 import ui.loginScreen
+import ui.homeScreen
 
 class App : Application() {
     override fun start(state: Map<String, Any>) {
         root("kvapp") {
-            showLogin()
+            showHome()
         }
     }
 }
@@ -17,7 +18,8 @@ private fun Container.showLogin() {
     removeAll()
     loginScreen(
         onLogin = { showStub("Вы вошли в систему") },
-        onGoToRegister = { showRegister() }
+        onGoToRegister = { showRegister() },
+        onGoHome = { showHome() }
     )
 }
 
@@ -25,11 +27,20 @@ private fun Container.showRegister() {
     removeAll()
     registrationScreen(
         onRegistered = { showStub("Регистрация успешна") },
-        onGoToLogin = { showLogin() }
+        onGoToLogin = { showLogin() },
+        onGoHome = { showHome() }
     )
 }
 
 private fun Container.showStub(msg: String) {
     removeAll()
     stubScreen(message = msg, onBack = { showLogin() })
+}
+
+private fun Container.showHome() {
+    removeAll()
+    homeScreen(
+        onGoToLogin = { showLogin() },
+        onGoToRegister = { showRegister() }
+    )
 }
---
+++ composeApp/src/jsMain/kotlin/ui/Buttons.kt
@@ -0,0 +1,12 @@
+﻿package ui
+
+import io.kvision.core.Container
+import io.kvision.core.onClick
+import io.kvision.html.div
+import io.kvision.html.image
+
+fun Container.homeIconButton(onHomeClick: () -> Unit) = div {
+    addCssClass("home-button")
+
+    this.onClick { onHomeClick() }
+}
